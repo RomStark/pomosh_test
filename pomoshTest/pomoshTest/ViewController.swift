3dee75fc275670e258b224581fd53b1d87f5d6e5
RomStark <rgtarkovskiy@edu.hse.ru>
2024-06-29 06:59:14 +0300

    init

---
+++ pomoshTest/pomoshTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pomoshTest
+//
+//  Created by Al Stark on 29.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
